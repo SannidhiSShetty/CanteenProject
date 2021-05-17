baaeb1932494dc0c1e1d28f6ec5ad088e719257a
sannidhi <sannidhisshetty98@gmail.com>
2021-05-17 10:45:12 +0530

    NewUpadate

---
+++ ProjectUdateRajath/app/customer.ts
@@ -0,0 +1,10 @@
+export class Customer {
+
+    public custId:number;
+    public custName:string;
+    public cusEmail:string;
+    public cusPassword:string;
+    public cusMobile:string;
+    public cusAddress:string;
+    constructor(){}
+}
